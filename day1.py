661b04b7a88d5c9bcdf79f128342b9a4784c8620
Dominique Danco <ddanco@gmail.com>
2024-12-01 21:38:53 -0500

    Minor improvements

--- day1.py
+++ day1.py
@@ -7,16 +7,15 @@ from typing import Dict, Tuple
 
 
 def sorted_lists(input: str) -> Tuple[Tuple[int, ...], Tuple[int, ...]]:
-  lines = input.split('\n')
-  first = tuple(int(line.split('   ')[0]) for line in lines)
-  second = tuple(int(line.split('   ')[-1]) for line in lines)
+  lines = tuple(line.split('   ') for line in input.split('\n'))
+  sortable_lines = tuple((int(a), int(b)) for (a, b) in lines)
+  first, second = zip(*sortable_lines)
 
   return (tuple(sorted(first)), tuple(sorted(second)))
 
 
 def total_distance(input: str) -> int:
   first_list, second_list = sorted_lists(input)
-  assert(len(first_list) == len(second_list))
   zipped = zip(first_list, second_list)
 
   difference = lambda pair: abs(pair[0] - pair[1])
@@ -30,16 +29,13 @@ def total_distance(input: str) -> int:
 
 
 def get_counts(sorted_list: Tuple[int, ...]) -> Dict[int, int]:
-  counts = {}
   assert len(sorted_list) > 0
-  cur = sorted_list[0]
-  count = 0
+  counts, cur, count = {}, sorted_list[0], 0
   for x in sorted_list:
     if x == cur:
       count += 1
     else:
-      counts[cur] = count
-      cur, count = x, 1
+      counts[cur], cur, count = count, x, 1
   counts[cur] = count
   return counts
 
@@ -48,12 +44,7 @@ def similarity_score(input: str) -> int:
   first_list, second_list = sorted_lists(input)
   counts = get_counts(second_list)
 
-  def similarity_value(number: int) -> int:
-    if number in counts:
-      return counts[number] * number
-    return 0
-
-  return sum(similarity_value(number) for number in first_list)
+  return sum(counts.get(number, 0) * number for number in first_list)
 
 
 ##############
@@ -61,7 +52,7 @@ def similarity_score(input: str) -> int:
 
 if __name__ == "__main__":
 
-  input = ...
+  input = '''...'''
 
   print(f'Total distance: {total_distance(input)}')
   print(f'Similarity score: {similarity_score(input)}')
