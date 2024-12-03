03226b5ed0b9c014b5aa3d133183cb6b9ef232ee
Dominique Danco <ddanco@gmail.com>
2024-12-03 17:53:40 -0500

    Tabs to spaces

--- day2.py
+++ day2.py
@@ -2,32 +2,32 @@ import copy
 
 
 def convert_level(raw_level: str) -> list[int]:
-	return [int(num) for num in raw_level.split(' ')]
+  return [int(num) for num in raw_level.split(' ')]
 
 def strictly_increasing_or_decreasing(level: list[int]) -> bool:
-	return len(level) == len(list(set(level))) and \
-			(level == sorted(level) or level == sorted(level, reverse=True))
+  return len(level) == len(list(set(level))) and \
+      (level == sorted(level) or level == sorted(level, reverse=True))
 
 def jumps_legal(level: list[int]) -> bool:
-	return not any(abs(level[i] - level[i+1]) > 3 for i in range(len(level) - 1))
+  return not any(abs(level[i] - level[i+1]) > 3 for i in range(len(level) - 1))
 
 def level_safe(level: list[int]) -> bool:
-	return strictly_increasing_or_decreasing(level) and jumps_legal(level)
+  return strictly_increasing_or_decreasing(level) and jumps_legal(level)
 
 def level_safe_with_dampening(level:list[int]) -> bool:
-	return any(level_safe(level[:i] + level[i+1:]) for i in range(len(level)))
+  return any(level_safe(level[:i] + level[i+1:]) for i in range(len(level)))
 
 ### Part 1 ###
 
 def safe_count(input: str) -> int:
-	levels = input.split('\n')
-	return len(list(filter(lambda level: level_safe(convert_level(level)), levels)))
+  levels = input.split('\n')
+  return len(list(filter(lambda level: level_safe(convert_level(level)), levels)))
 
 ### Part 2 ###
 
 def safe_count_with_dampening(input: str) -> int:
-	levels = input.split('\n')
-	return len(list(filter(lambda level: level_safe_with_dampening(convert_level(level)), levels)))
+  levels = input.split('\n')
+  return len(list(filter(lambda level: level_safe_with_dampening(convert_level(level)), levels)))
 
 ##############
 
