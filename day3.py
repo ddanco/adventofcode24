7944bdc8913ce9dc736e48cd02712f7fcb6138f7
Dominique Danco <ddanco@gmail.com>
2024-12-03 22:53:30 -0500

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -1,17 +1,37 @@
 import re
 
+from typing import Tuple
+
 
 def get_product(match: str) -> int:
-    split_elems = match[4:-1].split(',')
-    return int(split_elems[0]) * int(split_elems[1])
+  split_elems = match[4:-1].split(',')
+  return int(split_elems[0]) * int(split_elems[1])
+
+### Part 1 ###
 
 def calculate_uncorrupted(text: str) -> int:
-    matches = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", text)
-    return sum(get_product(match) for match in matches)
+  matches = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", text)
+  return sum(get_product(match) for match in matches)
 
+### Part 2 ###
+
+def calculate_enabled_multiplications(text: str) -> int:
+  def helper(params: Tuple[str, int]) -> Tuple[str, int]:
+    text, acc = params
+    stop_point = text.find("don't()")
+    next_start_point = text.find("do()", stop_point)
+    updated_sum = calculate_uncorrupted(text[:stop_point]) + acc
+    if next_start_point == -1:
+      return ('', updated_sum)
+    return helper((text[next_start_point:], updated_sum))
+  _, acc = helper((text + "don't()", 0))
+  return acc
+
+##############
 
 if __name__ == "__main__":
 
-    input = """..."""
+  input = '''...'''
 
-    print(f'Sum of uncorrupted products is: {calculate_uncorrupted(input)}')
+  print(f'Sum of uncorrupted: {calculate_uncorrupted(input)}')
+  print(f'Sum of enabled uncorrupted: {calculate_enabled_multiplications(input)}')
