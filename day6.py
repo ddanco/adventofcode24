f617684b596611a55f1f26e84284ef1faf271092
Dominique <ddanco@gmail.com>
2024-12-08 22:42:23 -0500

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -6,6 +6,38 @@ from typing import Tuple
 ### Part 1 ###
 ##############
 
+## Maximum recursion depth exceeded for full input :(
+# def fill_map_rec(grid: list[str], loc: Tuple[int, int]) -> list[str]:
+#   guard = grid[loc[0]][loc[1]]
+#   if loc[0] == 0 and guard == '^' or \
+#       loc[0] == len(grid) - 1 and guard == 'v' or \
+#       loc[1] == 0 and guard == '<' or \
+#       loc[1] == len(grid[0]) - 1 and guard == '>':
+#     grid[loc[0]] = grid[loc[0]].replace(guard, 'X')
+#     return grid
+
+#   next_guard_pos = {
+#       '^': (loc[0]-1, loc[1]),
+#       'v': (loc[0]+1, loc[1]),
+#       '>': (loc[0], loc[1]+1),
+#       '<': (loc[0], loc[1]-1)}
+#   next_pos = next_guard_pos[guard]
+
+#   rotations = {
+#       '^': '>',
+#       'v': '<',
+#       '>': 'v',
+#       '<': '^'}
+
+#   if grid[next_pos[0]][next_pos[1]] == '#':
+#     grid[loc[0]] = grid[loc[0]].replace(guard, rotations[guard])
+#     return fill_map_rec(grid, loc)
+
+#   grid[loc[0]] = grid[loc[0]][:loc[1]] + 'X' + grid[loc[0]][loc[1]+1:]
+#   grid[next_pos[0]] = grid[next_pos[0]][:next_pos[1]] + guard + grid[next_pos[0]][next_pos[1]+1:]
+#   return fill_map_rec(grid, next_pos)
+
+
 def fill_map_iter(grid: list[str]) -> list[str]:
 
   def complete(loc, guard) -> bool:
@@ -58,6 +90,8 @@ def get_loc_of_guard(grid: list[str]) -> Tuple[int, int]:
 
 def get_num_spaces_visited(input: str) -> int:
   grid_rows = input.split('\n')
+  # guard_loc = get_loc_of_guard(grid_rows)
+  # filled_map = fill_map_rec(grid_rows, guard_loc)
   filled_map = fill_map_iter(grid_rows)
   return len(tuple(filter(lambda char: char == 'X', chain(*filled_map))))
 
